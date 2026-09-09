8774be9c322d4a0a5d77f49366a4b3fea7db7d8b
Hanine Mounsi <hanine.mounsi23@gmail.com>
2026-09-09 15:18:23 +0200

    modif

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,12 @@
+
+import fonctions as f
+
 print("hello ,World!")
+
+
+chiffre1 = int(input("Entrez un chiffre : "))
+chiffre2 = int(input("Entrez un chiffre : "))
+
+res = f.puissance(chiffre1,chiffre2)
+
+print("le resultat est ",res)
