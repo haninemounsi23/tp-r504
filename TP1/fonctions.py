e9dbecc0fa506d3fca06e3d1abe339fc0483997a
Hanine Mounsi <hanine.mounsi23@gmail.com>
2026-09-16 14:19:02 +0200

    Ajout de la fonction sans l'operateur intégré

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,15 +1,10 @@
-	
-
-def puissance(a,b):
-	if not type(a) is int or not type(b) is int :
-		raise TypeError("Only integers are allowed")
-	else :
-
- 		res = 1
+def puissance(a, b):
+    if not type(a) is int or not type(b) is int:
+        raise TypeError("Only integers are allowed")
+    else:
+        res = 1
 
         for i in range(b):
-
             res = res * a
-        
-		return res
 
+        return res
