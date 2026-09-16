0bea2584b08dc1d3602e81918e3003a80bad8720
Hanine Mounsi <hanine.mounsi23@gmail.com>
2026-09-16 15:46:31 +0200

    modif

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,15 +1,18 @@
 def puissance(a, b):
     if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
+
+    if a == 0 and b == 0:
+        raise ValueError("0 puissance 0 est indéfini")
+
+    res = 1
+
+    if b >= 0:
+        for i in range(b):
+            res = res * a
     else:
-        res = 1
+        for i in range(-b):
+            res = res * a
+        res = 1 / res
 
-        if b >= 0:
-            for i in range(b):
-                res = res * a
-        else:
-            for i in range(-b):
-                res = res * a
-            res = 1 / res
-
-        return res
+    return res
